a689781706caa60449597854d7bd84b3c27b0629
Suttekh <josueferrrreira@hotmail.com>
2020-10-16 16:39:07 -0300

    Titano

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu Ovo</title>
+</head>
+<body>
+    <h1>Titanoboa moideno :3</h1>
+    <img src="img/titanoboa.jpg" alt="">
+</body>
+</html>
